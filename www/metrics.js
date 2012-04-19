f2c4f453006a2a155305c283bcbe4461402ee498
Mathieu Lecarme <mathieu@garambrogne.net>
2012-04-20 00:18:46 +0200

    Round and css id escape.

--- www/metrics.js
+++ www/metrics.js
@@ -3,15 +3,18 @@ $(function() {
     var table = $('table');
 
     var stocks = new EventSource('/events');
+    function escape(e) {
+        return e.replace(/[#.]/g, '_');
+    }
     stocks.onmessage = function(event) {
         var data = JSON.parse(event.data); // read the event as JSON
         for (var k in data) {
-            var line = $('#' + k, table);
+            var line = $('#' + escape(k), table);
             if (line.length) { // the line exists
-                line.text(data[k]);
+                line.text(Math.round(data[k]) + ' %');
             } else {
-                table.append('<tr><td>' + k + '</td><td id="' + k + '">' +
-                    data[k] + '</td></tr>');
+                table.append('<tr><td>' + k + '</td><td id="' + escape(k) + '">' +
+                    Math.round(data[k]) + '% </td></tr>');
             }
         }
     };
